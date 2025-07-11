95aabe319c4f2c0442f8c4a375ca4c8cd2f56491
Thiago Ozores <tozores@gmail.com>
2025-07-11 00:03:37 +0000

    Feat/deploy

---
+++ infra/variables.tf
@@ -0,0 +1,21 @@
+variable "render_api_key" {
+  description = "The API key for the Render account."
+  type        = string
+  sensitive   = true
+}
+
+variable "render_owner_id" {
+  description = "The owner ID for the Render account."
+  type        = string
+}
+
+variable "docker_image_url" {
+  description = "The URL of the Docker image to deploy."
+  type        = string
+}
+
+variable "service_name" {
+  description = "The name of the Render service."
+  type        = string
+  default     = "my-docker-app"
+}
